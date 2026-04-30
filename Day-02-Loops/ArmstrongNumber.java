1955d95d3815308c7cdb04424c16e4d130eacb69
Amrita Shivhare <shivhareamrita468@gmail.com>
2026-04-30 19:38:39 +0530

    Create ArmstrongNumber.java

---
+++ Day-02-Loops/ArmstrongNumber.java
@@ -0,0 +1,17 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153, temp = num, sum = 0;
+
+        while(temp != 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if(sum == num)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
+//Armstrong
