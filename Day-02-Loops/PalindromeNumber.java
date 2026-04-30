1e95acafad67d22701c0d11d1842968152886591
Amrita Shivhare <shivhareamrita468@gmail.com>
2026-04-30 17:46:03 +0530

    Create PalindromeNumber.java

---
+++ Day-02-Loops/PalindromeNumber.java
@@ -0,0 +1,18 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int num = 121, temp = num, rev = 0;
+
+        while(temp != 0) {
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp /= 10;
+        }
+
+        if(num == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
+
+//Palindrome
