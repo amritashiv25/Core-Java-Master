c3cd63ce9d2dc3bfe50d35001f594473789a1f68
Amrita Shivhare <shivhareamrita468@gmail.com>
2026-04-30 19:38:01 +0530

    Create MultiplicationTable.java

---
+++ Day-02-Loops/MultiplicationTable.java
@@ -0,0 +1,15 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
+/* 
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50 
+  */
