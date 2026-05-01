0fab832dfa3581a0ad99765aa846581ac3ecf0d7
Amrita Shivhare <shivhareamrita468@gmail.com>
2026-05-01 20:24:21 +0530

    Create Count Elements.java

---
+++ Day-04-Arrays-Program/Count Elements.java
@@ -0,0 +1,8 @@
+public class CountElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Total elements = " + arr.length);
+    }
+}
+//Total elements = 5
